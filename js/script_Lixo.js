226439a7d0d32735222ec261f894a4eddd0d3906
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-26 15:33:17 -0300

    script.js alterado

--- js/script_Lixo.js
+++ js/script_Lixo.js
@@ -1,12 +1,39 @@
-import { RecolherLixo } from "./script.js";
+
+const RecolherLixo = (objLixo) => {
+    const TonLixo = 220;
+    const ValordoLixo = TonLixo * parseInt(objLixo.lixo);
+
+    let CotaDeLixo = false;
+    let premio = 0;
+
+    if (objLixo.lixo < 1000) {
+        CotaDeLixo = true;
+    } else {
+        if(objLixo.lixo >= 1000 && objLixo.lixo < 10000 ){
+            premio = ValordoLixo * 0.08;
+        } else if(objLixo.lixo >= 10000 && objLixo.lixo < 15000){
+            premio = ValordoLixo * 0.10;
+        } else if(objLixo.lixo >= 15000 && objLixo.lixo < 25000){
+            premio = ValordoLixo * 0.15;
+        } else{
+            premio = ValordoLixo * 0.20;
+        }
+    }
+
+    const ResultadoFinal = ValordoLixo + (CotaDeLixo ? 0 : premio);
+
+    return {
+        valor: ValordoLixo,
+        premiodolixo: CotaDeLixo ? 0 : premio,
+        total: ResultadoFinal
+    };
+};
 
 const FormEmpresa = document.querySelector('#dados-empresa');
-
 const tabela = document.querySelector('#tabela');
-
 const empresas = [];
 
-FormEmpresa.addEventListener('submit',(evt) => {
+FormEmpresa.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
     const dadosEmpresa = new FormData(FormEmpresa);
@@ -16,6 +43,7 @@ FormEmpresa.addEventListener('submit',(evt) => {
         nome: dadosEmpresa.get('nome'),
         lixo: dadosEmpresa.get('lixo')
     };
+    
     addEmpresa(empresa);
     FormEmpresa.reset();
 });
@@ -28,26 +56,21 @@ const addEmpresa = (objLixo) => {
 const listEmpresas = () => {
     tabela.innerHTML = '';
 
-    empresas.forEach((elem, i) => {
+    empresas.forEach((elem) => {
         const resultCalculo = RecolherLixo(elem);
 
         const valorFormatado = resultCalculo.valor.toFixed(2).replace('.', ',');
         const PremioFormatado = resultCalculo.premiodolixo.toFixed(2).replace('.', ',');
         const resultadoEnd = resultCalculo.total.toFixed(2).replace('.', ',');
 
-        // Substitua tbodyLista por tabela novamente
         tabela.innerHTML += `
             <tr>
                 <td>${elem.nome}</td>
                 <td>${elem.lixo}</td>
-                <td>${valorFormatado}</td>
-                <td>${PremioFormatado}</td>
-                <td>${resultadoEnd}</td>
+                <td>R$ ${valorFormatado}</td>
+                <td>R$ ${PremioFormatado}</td>
+                <td>R$ ${resultadoEnd}</td>
             </tr>
         `;
     });
-}
-    
-
-    
-    
+};
