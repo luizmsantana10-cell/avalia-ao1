3645b5c100e753f64b6c5d67d19a6b34beb79509
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-26 15:00:38 -0300

    script_Lixo alterado

---
+++ js/script_Lixo.js
@@ -0,0 +1,37 @@
+import { RecolherLixo } from "./script.js";
+
+const FormEmpresa = document.querySelector('#dados-empresa');
+
+const tabela = document.querySelector('#tabela');
+
+const empresas = [];
+
+FormEmpresa.addEventListener('submit',(evt) => {
+    evt.preventDefault();
+
+    const dadosEmpresa = new FormData(FormEmpresa);
+
+    const empresa = {
+        nome: dadosEmpresa.get('nome')
+        lixo: dadosEmpresa.get(lixo)
+    };
+    addEmpresa(empresa);
+    FormEmpresa.reset();
+});
+
+const addEmpresa = (objLixo) => {
+    empresas.push(objLixo);
+    listEmpresas();
+};
+
+const listEmpresas = () => {
+    tbodyLista.innerHTML = '';
+
+    empresas.forEach((elem, i) => {
+        const resultCalculo = RecolherLixo(elem);
+
+        const valorFormatado = resultCalculo.valor.toFixed(2).replace('.', ',');
+        const PremioFormatado = resultCalculo.premiodolixo.toFixed(2).replace('.', ',');
+
+        const resultadoEnd = resultCalculo.total.toFixed(2).replace('.', ',');
+}
